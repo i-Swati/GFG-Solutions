cd1cb6485570a8c247a544288d76558cc1d4dc83
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2023-09-20 20:36:57 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Missing number in array/missing-number-in-array.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+public:
+    int missingNumber(vector<int>& arr, int n) {
+        int XOR1 = 0;
+        for (int i = 0; i <= n; i++) {
+            XOR1 = XOR1 ^ i;
+        }
+        
+        int XOR2 = 0;
+        for (int num : arr) {
+            XOR2 = XOR2 ^ num;
+        }
+
+        return XOR1 ^ XOR2;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.missingNumber(array, n) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
