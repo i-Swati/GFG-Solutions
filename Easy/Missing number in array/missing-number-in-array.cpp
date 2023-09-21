bbb8d228766bf0ad1440cb16068f25b4fe594ebc
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2023-09-21 14:20:25 +0530

    Updated Solution - GfG to GitHub

--- Easy/Missing number in array/missing-number-in-array.cpp
+++ Easy/Missing number in array/missing-number-in-array.cpp
@@ -17,7 +17,8 @@ public:
         for (int i = 0; i <= n; i++) {
             XOR1 = XOR1 ^ i;
         }
-        
+        //instead of a traditional for loop, it is a range-based loop used to iterate through 
+        //the elemnts of the arr vector directly 
         int XOR2 = 0;
         for (int num : arr) {
             XOR2 = XOR2 ^ num;
